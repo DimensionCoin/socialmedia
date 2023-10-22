0d92eff83b6194bd0103fd0b907d73c468f7ee0d
DimensionCoin <nick.vassallo97@gmail.com>
2023-10-22 17:31:04 -0400

    added cors

--- backend/server.js
+++ backend/server.js
@@ -25,6 +25,14 @@ app.use(express.json({ limit: "50mb" })); // To parse JSON data in the req.body
 app.use(express.urlencoded({ extended: true })); // To parse form data in the req.body
 app.use(cookieParser());
 
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "https://nicksocial.app"],
+  })
+);
+
+
+
 // Routes
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
